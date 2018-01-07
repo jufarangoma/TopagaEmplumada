4765ec13d55b0a5c55e0af2f14852cd3495f6af8
Juan Arango <jufarangoma@unal.edu.co>
2018-01-06 22:40:16 -0500

    Creation of the application

---
+++ app/src/main/java/com/topagaemplumada/jufarangoma/tpagaemplumada/Models/Bird.kt
@@ -0,0 +1,12 @@
+package com.topagaemplumada.jufarangoma.tpagaemplumada.Models
+
+/**
+ * Created by Juan Felipe Arango on 1/6/18.
+ */
+class Bird{
+    var name: String?=null
+    var scientificName:String?=null
+    var photo: String?=null
+    var audio: Any?=null
+    var description: String?=null
+}
